23bbd7605cb3b3fa6f429dc9fa063e3079080b5b
Ludovic Pouillie <ludovicpouillie@gmail.com>
2024-10-15 11:16:38 +0200

    ajout des noms en couleur

---
+++ index.js
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+
+"Alex";
+console.log(chalk.blue('Alex'));
+
+"Simon";
+console.log(chalk.green('Simon'));
+
+"Badre";
+console.log(chalk.red('Badre'));
+
+"Nicolas";
+console.log(chalk.yellow('Nicolas'));
